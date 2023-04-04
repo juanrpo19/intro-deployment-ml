d245d0cde2d1a9b37009fb3f2d89b2746beb927c
Juanrpo19 <juanrpo19@hotmail.com>
2023-04-03 20:33:56 -0500

    print setter

--- utilities/setter.py
+++ utilities/setter.py
@@ -5,7 +5,7 @@ def main():
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
     with open('path.json', 'w') as json_file:
         json_file.write(b64decode(key).decode())
-    print(os.path.realpath('path.json'))
+    print(">>", os.path.realpath('path.json'))
 
 if __name__ == '__main__':
     main()
