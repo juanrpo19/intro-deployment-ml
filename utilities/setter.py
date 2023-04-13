5963e764515610b572255688420a9064a2edef7e
Juanrpo19 <juanrpo19@hotmail.com>
2023-04-12 20:20:54 -0500

    Adding encoding in key setter

--- utilities/setter.py
+++ utilities/setter.py
@@ -3,8 +3,8 @@ from base64 import b64decode
 
 def main():
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
-    with open('path.json', 'w', encoding='utf-8') as json_file:
-        json_file.write(b64decode(key).decode())
+    with open('path.json', 'w') as json_file:
+        json_file.write(b64decode(key).decode('utf-8'))
     print(os.path.realpath('path.json'))
 
 if __name__ == '__main__':
