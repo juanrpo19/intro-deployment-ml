2ba7a5ff975cc8b8e925c9932252deec77b67542
Juanrpo19 <juanrpo19@hotmail.com>
2023-04-13 19:10:12 -0500

    Change model name and publish metrics

--- api/app/utils.py
+++ api/app/utils.py
@@ -15,7 +15,7 @@ from io import BytesIO
 def get_model() -> Pipeline:
     # model_path = os.environ.get('MODEL_PATH', 'model/model.pkl')
     # with open(model_path, 'rb') as model_file:
-    model = joblib.load('model/model2.pkl')
+    model = joblib.load('model/model.pkl')
     return model
     
 def transform_to_dataframe(class_model: BaseModel) -> DataFrame:
