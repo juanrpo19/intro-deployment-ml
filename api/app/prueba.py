95c2efc3692ed74c23d7aee99946a87dcc4723a0
Juanrpo19 <juanrpo19@hotmail.com>
2023-04-17 19:33:17 -0500

    Change pandas version

--- api/app/prueba.py
+++ api/app/prueba.py
@@ -1,11 +1,12 @@
-import joblib
+# import joblib
+from utils import get_model
 import pandas as pd
 
-model = joblib.load('./model/model.pkl')
+json_pred = {"opening_gross": 17435092, "screens": 3008, "production_budget": 65000000, "title_year": 2012, "aspect_ratio": 2.35, "duration": 99, "cast_total_facebook_likes": 1375, "budget": 65000000, "imdb_score": 6.4}
 
+model = get_model()
 
-json_pred = {"opening_gross": 17435092.0, "screens": 3008.0, "production_budget": 65000000, "title_year": 2012.0, "aspect_ratio": 2.35, "duration": 99.0, "cast_total_facebook_likes": 1375, "budget": 65000000.0, "imdb_score": 6.4}
-
-df = pd.DataFrame(json_pred)
-
-model.predict(df)
+df = pd.DataFrame([json_pred])
+print(df)
+# breakpoint()
+print(model.predict(df))
--- api/app/views.py
+++ api/app/views.py
@@ -5,7 +5,5 @@ model = get_model()
 
 def get_prediction(request: PredictionRequest)-> float:
     data_to_predict = transform_to_dataframe(request)
-
-
     prediction = model.predict(data_to_predict)[0]
     return max(0, prediction)
