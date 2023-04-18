f5f434bb9c6bd32e9a27df01e229c4d42201b977
Juanrpo19 <juanrpo19@hotmail.com>
2023-04-17 19:38:15 -0500

    Change loading pkl

--- api/app/utils.py
+++ api/app/utils.py
@@ -1,4 +1,4 @@
-import joblib
+
 from joblib import load
 from sklearn.pipeline import Pipeline
 from pydantic import BaseModel
@@ -13,10 +13,14 @@ from io import BytesIO
 #     return model
     
 def get_model() -> Pipeline:
+    model_path = os.environ.get('MODEL_PATH','model/model.pkl')
+    with open(model_path,'rb') as model_file:
+        model = load(BytesIO(model_file.read()))
+    return model
     # model_path = os.environ.get('MODEL_PATH', 'model/model.pkl')
     # with open(model_path, 'rb') as model_file:
-    model = joblib.load('model/model.pkl')
-    return model
+    # model = joblib.load('model/model.pkl')
+    # return model
     
 def transform_to_dataframe(class_model: BaseModel) -> DataFrame:
     transition_dictionary = {key:[value] for key, value in class_model.dict().items()}
